223e8dfc55f8a3020dfb49ed95006ddb38b7742c
Salmasrj <89201396+Salmasrj@users.noreply.github.com>
2025-03-03 20:56:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install -y graphviz
+
 pip install librairie/grapheMPM-0.7.2-py3-none-any.whl
+sed -i 's/from numpy import asarray, prod, float_, round, floor, uint8/from numpy import asarray, prod, float16, round, floor, uint8/' $(python -c "import site; print(site.getsitepackages()[0])")/grapheMPM/__init__.py
